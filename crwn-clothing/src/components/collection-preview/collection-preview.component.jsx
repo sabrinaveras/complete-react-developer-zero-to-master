1de251e1356a76f3976ff8e28ffe0af2a2cfdf6c
Sabrina <sabrina_veras@live.com>
2019-11-11 11:33:48 -0300

    collection preview done

--- crwn-clothing/src/components/collection-preview/collection-preview.component.jsx
+++ crwn-clothing/src/components/collection-preview/collection-preview.component.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
-import "./colletion-preview.styles.scss";
+import CollectionItem from "../collection-item/collection-item.component";
+
+import "./collection-preview.styles.scss";
 
 const ColletionPreview = ({ title, items }) => (
    <div className="collection-preview">
         <h1 className="title">{title.toUpperCase()}</h1>
         <div className="preview">
              {
-                  items.map(item => (
-                     <div key={item.id} >{item.name}</div>
+                  items
+                     .filter((item, idx) => idx < 4)
+                     .map(({id, ...otherItemProps}) => (
+                     <CollectionItem key={id} {...otherItemProps} />
                   ))
              }
         </div>
