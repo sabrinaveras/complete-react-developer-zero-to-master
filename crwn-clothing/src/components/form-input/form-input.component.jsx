3c49ab5c110716a44e8213ca75d3032c5d9f5a85
Sabrina Veras <sabrina_veras@live.com>
2020-04-29 17:26:39 -0300

    comments sign-in

--- crwn-clothing/src/components/form-input/form-input.component.jsx
+++ crwn-clothing/src/components/form-input/form-input.component.jsx
@@ -3,16 +3,22 @@ import React from "react";
 import "./form-input.styles.scss";
 
 const FormInput = ({handleChange, label, ...otherProps}) =>(
+    
      <div className="group">
+         
           <input className="form-input" onChange={handleChange} {...otherProps} />
+          
           {
+              //  check if exist a label
                label ?
                   (<label className={`${otherProps.value.length ? "shrink" : "" } form-input-label `} >
                        {label}
                   </label>)
                   : null
           }
+          
      </div>
+    
 );
 
 
--- crwn-clothing/src/components/sign-in/sign-in.component.jsx
+++ crwn-clothing/src/components/sign-in/sign-in.component.jsx
@@ -17,12 +17,26 @@ class SignIn extends React.Component{
           }
      }
 
+     /*
+     *  Submit function that calls our method handleSubmit which for now will simply prevent the default submit action
+     *  from firing because we want full control over exactly what this summit is going to do.
+     * */
      handleSubmit = event =>{
           event.preventDefault();
 
           this.setState({email: "", password: ""})
      };
 
+     /*
+     *  handleChange (OnChange) function which also again takes an event except for this were going to pull both the value and the name
+     *  off of our event dot target the event target will end up being our input element itself and we want the name value
+     *  that we just set as well as the actual value on that target that they've just typed in and whats we're going to do
+     *  is dynamically ser our state so that it sets that name.
+     *
+     *  So it'll render whatever that value that comes in will be if name is password. It will say password and that point
+     *  to the value that they typed in and then the same will apply for email if it's the email then this event targets
+     *  dot name will be email and then the value will what they're typing in there.
+     * */
      handleChange = event =>{
        const {value, name} = event.target;
 
