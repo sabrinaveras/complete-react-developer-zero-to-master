42648bb6c3fc6546e8e941fb172ad90813e37870
Sabrina Veras <sabrina_veras@live.com>
2020-05-05 17:37:30 -0300

    redux cart dropdown

--- crwn-clothing/src/components/header/header.component.jsx
+++ crwn-clothing/src/components/header/header.component.jsx
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom";
 import { auth } from "../../firebase/firebase.utils";
 
 import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 /*
@@ -19,7 +20,7 @@ import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import "./header.styles.scss";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
    <div className="header">
        
         <Link to="/" className="logo-container">
@@ -43,13 +44,17 @@ const Header = ({ currentUser }) => (
              
              <CartIcon/>
         </div>
+       
+       { hidden ? null: <CartDropdown/>}
+       
    </div>
 );
 
 
 //  This naming can be anything but mapStateToProps is standard with redux codebases
-const mapStateToProps = state =>({
-   currentUser: state.user.currentUser
+const mapStateToProps = ( {user: {currentUser}, cart: { hidden }} ) =>({
+    currentUser,
+    hidden
 });
 
 
