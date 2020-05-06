a836f8f36694c9a62929f90edd2069325de36d75
Sabrina Veras <sabrina_veras@live.com>
2020-05-06 17:20:45 -0300

    updating our application with reselect, replaced cart selector with memoized selectors

--- crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import { connect } from "react-redux";
 
+import { selectCartItems } from "../../redux/cart/cart.selector";
+
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
 
@@ -24,8 +26,8 @@ const CartDropdown = ({ cartItems }) =>(
 )
 
 
-const mapStateToProps = ({cart: { cartItems  }}) => ({
-	cartItems
+const mapStateToProps = state => ({
+	cartItems: selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropdown);
---
+++ crwn-clothing/src/redux/cart/cart.selector.js
@@ -0,0 +1,25 @@
+import { createSelector } from "reselect";
+
+const selectCart = state => state.cart;
+
+
+export const selectCartItems = createSelector([selectCart], (cart) => cart.cartItems);
+
+export const selectCartItemsCount = createSelector([selectCartItems], cartItems => cartItems.reduce(
+	(accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0
+))
+
+
+
+/*
+*   We have to switch up our folder structure a little bit because we are going to move the code that we normally write
+*   inside of the respective redux.
+*
+*   That this will be related to. So for example this car item count might be a reusable selector in the sense that we
+*   might want an item count in multiple places of our application and error for us to do that. WE would add it into a new
+*   file called cart.selector.js
+*
+*
+*
+*
+* */
