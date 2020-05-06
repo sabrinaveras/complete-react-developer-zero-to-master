b7f5fb1972162abcb55c350b3192ade44de43ed8
Sabrina Veras <sabrina_veras@live.com>
2020-05-06 17:57:55 -0300

    checkout page part two

--- crwn-clothing/src/redux/cart/cart.selector.js
+++ crwn-clothing/src/redux/cart/cart.selector.js
@@ -11,6 +11,10 @@ export const selectCartItemsCount = createSelector([selectCartItems], cartItems 
 	(accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0
 ))
 
+export const selectCartTotal = createSelector([selectCartItems], cartItems => cartItems.reduce(
+	(accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity * cartItem.price, 0
+))
+
 
 
 /*
