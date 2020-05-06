f19511e0363c4000e3fb7b21b3f4f354043c9770
Sabrina Veras <sabrina_veras@live.com>
2020-05-06 18:27:52 -0300

    remove items from shopping bag in checkout page

--- crwn-clothing/src/redux/cart/cart.actions.js
+++ crwn-clothing/src/redux/cart/cart.actions.js
@@ -10,3 +10,8 @@ export const addItem = item =>({
 	type: CartActionTypes.ADD_ITEM,
 	payload: item
 });
+
+export const clearItemFromCart = item =>({
+	type: CartActionTypes.CLEAN_ITEM_FROM_CART,
+	payload: item
+});
--- crwn-clothing/src/redux/cart/cart.types.js
+++ crwn-clothing/src/redux/cart/cart.types.js
@@ -1,6 +1,7 @@
 const CartActionTypes = {
 	TOGGLE_CART_HIDDEN: "TOGGLE_CART_HIDDEN",
-	ADD_ITEM: "ADD_ITEM"
+	ADD_ITEM: "ADD_ITEM",
+	CLEAN_ITEM_FROM_CART: "CLEAN_ITEM_FROM_CART"
 }
 
 export default CartActionTypes;
